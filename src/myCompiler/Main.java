e4cd238cd6441367426c78dae430005aa4f54fd2
Dovydas Girsvaldas <dovydas.girsvaldas@gmail.com>
2016-12-21 21:55:38 +0100

    deleted drawable fodler

--- src/myCompiler/Main.java
+++ src/myCompiler/Main.java
@@ -14,7 +14,6 @@ public class Main extends Application {
         try {
             Parent root= FXMLLoader.load(getClass().getResource("layout/EclipseMain.fxml"));
             Scene scene = new Scene(root);
-            System.out.println("### comment for git");
            // scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
             primaryStage.setScene(scene);
             primaryStage.show();
